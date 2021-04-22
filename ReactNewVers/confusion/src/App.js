29eb837d945102e9bd994f0b99e13811577d2196
Ivan Ayala <ivan.ayala.alcalde@gmail.com>
2021-04-22 14:02:03 +0200

    Introduction to Redux

--- ReactNewVers/confusion/src/App.js
+++ ReactNewVers/confusion/src/App.js
@@ -2,16 +2,21 @@ import './App.css';
 import React from 'react';
 import Main from './components/Main'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
 
+const store = ConfigureStore();
 
-function App() {
-    return (
+function App() {  
+  return (
+    <Provider store={store}>
       <BrowserRouter>
         <div>
           <Main/>
         </div>
       </BrowserRouter>
-    );
+    </Provider>
+  );
 }
 
 export default App;
--- ReactNewVers/confusion/src/components/Main.js
+++ ReactNewVers/confusion/src/components/Main.js
@@ -5,74 +5,68 @@ import Footer from './Footer';
 import Menu from './Menu';
 import About from './AboutComponent';
 import Contact from './ContactComponent';
-//import DishDetail from './DishDetail';
-import {DISHES} from '../shared/dishes';
-import {COMMENTS} from '../shared/comments';
-import {PROMOTIONS} from '../shared/promotions';
-import {LEADERS} from '../shared/leaders';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import DishDetail from './DishDetail';
+import { connect } from 'react-redux';
 
-class Main extends Component {
+const mapStateToProps = state => {
+    return {
+        dishes: state.dishes,
+        comments: state.comments,
+        promotions: state.promotions,
+        leaders: state.leaders
+    }   
+}
+
+function Main(props) {
     // Component lifecycle
     // 1. Constructor
     // 2. Render
     // 3. componentDidMount
     // 4. and above: Renders depending on events (clic, etc)
+    
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            dishes: DISHES,
-            comments: COMMENTS,
-            promotions: PROMOTIONS,
-            leaders: LEADERS
-        };
-    }
-
-    getFeatured(list) {
+    function getFeatured(list) {
         return list.filter((element) => element.featured)[0]; // First element that has "featured" equal to True
     }
 
-    render() {
-        // Anything that does not match or menu will be redirect to /home (redirect)
-        const HomePage = () => {
-            return (
-                <Home 
-                    dish={this.getFeatured(this.state.dishes)} 
-                    leader={this.getFeatured(this.state.leaders)}
-                    promotion={this.getFeatured(this.state.promotions)}
-                />
-            );
-        }
-        
-        // Remember that React routers also passes location and history, but we'll only use match here
-        // parseInt(match.params.dishId,10) ---> Convert string to a based-10 integer
-        const DishWithId = ({match}) => {
-            return (
-                <DishDetail 
-                    dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
-                    comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
-                >
-                </DishDetail>
-            );
-        }
-        
+    // Anything that does not match or menu will be redirect to /home (redirect)
+    const HomePage = () => {
         return (
-            <div>
-                <Header />
-                <Switch>
-                    <Route path="/home" component={HomePage}></Route>
-                    <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>}></Route>
-                    <Route path="/menu/:dishId" component={DishWithId}></Route>
-                    <Route exact path="/contactus" component={Contact} />
-                    <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders}/>}></Route>
-                    <Redirect to="/home" ></Redirect>
-                </Switch>
-                <Footer />
-            </div>
+            <Home 
+                dish={getFeatured(props.dishes)} 
+                leader={getFeatured(props.leaders)}
+                promotion={getFeatured(props.promotions)}
+            />
         );
     }
+    
+    // Remember that React routers also passes location and history, but we'll only use match here
+    // parseInt(match.params.dishId,10) ---> Convert string to a based-10 integer
+    const DishWithId = ({match}) => {
+        return (
+            <DishDetail 
+                dish={props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+                comments={props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+            >
+            </DishDetail>
+        );
+    }
+    
+    return (
+        <div>
+            <Header />
+            <Switch>
+                <Route path="/home" component={HomePage}></Route>
+                <Route exact path="/menu" component={() => <Menu dishes={props.dishes}/>}></Route>
+                <Route path="/menu/:dishId" component={DishWithId}></Route>
+                <Route exact path="/contactus" component={Contact} />
+                <Route exact path="/aboutus" component={() => <About leaders={props.leaders}/>}></Route>
+                <Redirect to="/home" ></Redirect>
+            </Switch>
+            <Footer />
+        </div>
+    );
 }
 
-export default Main;
+export default withRouter(connect(mapStateToProps)(Main));
